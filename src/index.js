ff226d9b5461b30a779f813d8750d4d80acd37b8
not-zendaya <ukzendaya@gmail.com>
2024-10-20 19:33:28 +0300

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -12,15 +12,15 @@ function generateRecipe(event) {
   event.preventDefault();
   let userInput = document.querySelector("#user-input");
   let apiKey = "c73bde3obcbtea944679949f5e0620ff";
-  let prompt = ` user instructions:Generate a recipe for ${userInput}`;
+  let prompt = ` user instructions:Generate a recipe for ${userInput.value}`;
 
   let context =
-    "You have very many recipes.Your mission is to generate simple recipe in basic HTML listing the recipe heading then the ingredients then the instructions.Make sure to follow users instructions.Do not add other stuff other than the recipe only.Do not include the `html```and the ``` after signing.Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe.";
+    "You have very many recipes.Your mission is to generate simple short recipe in basic HTML listing the recipe heading then the ingredients then the instructions.Make sure to follow users instructions.Do not add other stuff other than the recipe only and reduce the spaces between the headings and lines to something more appealing.Do not include the `html```and the ``` after signing.Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating recipe....");
-  console.log(`${prompt}`);
-  console.log(`${context}`);
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">⏳ Generating a recipe about ${userInput.value}</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
